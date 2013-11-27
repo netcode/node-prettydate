d4db971a65742555c04fc02eadf74edd90ade180
sergeyt <stodyshev@gmail.com>
2013-11-27 22:37:14 +0700

    fixed circular dependency

--- package.js
+++ package.js
@@ -1,9 +1,8 @@
 Package.describe({
-  summary: "format old JavaScript dates in a “pretty” way. ex : 2 hours ago , 3 minutes ago."
+  summary: "format old JavaScript dates in a ï¿½prettyï¿½ way. ex : 2 hours ago , 3 minutes ago."
 });
 
 Package.on_use(function (api) {
-  api.use('prettydate', ['client', 'server']);
   if (api.export) api.export('PrettyDate');
   api.add_files('index.js', ['client', 'server']);
 });
