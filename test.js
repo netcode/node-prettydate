25576e63fde8e48e97ad92246bd79f595b2f8af6
Jakub Mandula <jakub.aludnam@gmail.com>
2014-10-05 21:14:06 +0200

    add time in future

--- test.js
+++ test.js
@@ -1,5 +1,18 @@
 var pd = require("./index");
 
+
+console.log( pd.format( new Date( new Date() - -2*365*24*60*60*1000 )));
+console.log( pd.format( new Date( new Date() - -365*24*60*60*1000 )));
+console.log( pd.format( new Date( new Date() - -2*30*24*60*60*1000 )));
+console.log( pd.format( new Date( new Date() - -30*24*60*60*1000 )));
+console.log( pd.format( new Date( new Date() - -2*24*60*60*1000 )));
+console.log( pd.format( new Date( new Date() - -24*60*60*1000 )));
+console.log( pd.format( new Date( new Date() - -2*60*60*1000 )));
+console.log( pd.format( new Date( new Date() - -60*60*1000 )));
+console.log( pd.format( new Date( new Date() - -2*60*1000 )));
+console.log( pd.format( new Date( new Date() - -60*1000 )));
+console.log( pd.format( new Date( new Date() - -2*1000 )));
+console.log( pd.format( new Date( new Date() - -1*1000 )));
 console.log( pd.format( new Date()));
 console.log( pd.format( new Date( new Date() - 1*1000 )));
 console.log( pd.format( new Date( new Date() - 2*1000 )));
